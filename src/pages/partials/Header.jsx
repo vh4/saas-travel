776e652e2cd6c9ed389bee273c12bd82baabf2e4
Fathoni Jati <fathoni.jati@corp.bm.co.id>
2022-12-23 13:11:08 +0700

    update logo navbar

--- src/pages/partials/Header.jsx
+++ src/pages/partials/Header.jsx
@@ -2,23 +2,27 @@
 
 import { Link } from "react-router-dom";
 import React, { useCallback } from 'react'
-
+import {BsBoxArrowRight} from 'react-icons/bs'
+import Logo from '../logo.png'
 
 export default function Header({toogleSidebar, valueSidebar}){
 
     const toggleFuction = useCallback( () => {
-                toogleSidebar(valueSidebar == "block" ? "hidden" : "block")
+                toogleSidebar(valueSidebar === "block" ? "hidden" : "block")
           }, [toogleSidebar, valueSidebar]);
 
     return(
         <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
+        <Link to={'/'} className="flex items-center">
+            <img src={Logo} className="w-full mr-3 sm:h-9" alt="Flowbite Logo" />
+            <span className="self-center text-xl text-slate-700 font-semibold whitespace-nowrap dark:text-white">Fastravel</span>
+        </Link>
         <div className="flex md:order-2">
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+            <button type="button" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-3.5 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white dark:border-gray-700 dark:text-gray-900 dark:hover:bg-gray-200 mr-2 mb-2">
+                <BsBoxArrowRight size={16} fontSize={"bold"} />
+                <div className="ml-2">Login</div>
+            </button>
             <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
                 <button onClick={toggleFuction}>
