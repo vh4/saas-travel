1c1a2e66896681bd7a45435a99de71226e3a9081
Silvi Rahmasari <silviarahmasari222@gmail.com>
2022-12-23 16:46:55 +0700

    add menu

--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -1,5 +1,58 @@
 export default function Menu(){
     return (
-        <></>
+        <>
+            <div className="row">
+                <div className="grid grid-cols-6 gap-2 pb-4 pr-5 object-center">
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="pulsadata.png" alt="" />
+                        <p className="text-xs">Pulsa & Data</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="transfer.png" alt="" />
+                        <p className="text-xs">Tranfer Antar Bank</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="trfspesial.png" alt="" />
+                        <p className="text-xs">Transfer Bank Special</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="ecommerce.png" alt="" />
+                        <p className="text-xs">E-Commerce</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="emoney.png" alt="" />
+                        <p className="text-xs">Top Up E-Money</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="game.png" alt="" />
+                        <p className="text-xs">Voucher Game</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="pln.png" alt="" />
+                        <p className="text-xs">PLN</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="djkm.png" alt="" />
+                        <p className="text-xs">Penerimaan Negara MPN</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="pdam.png" alt="" />
+                        <p className="text-xs">PDAM</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="bpjs.png" alt="" />
+                        <p className="text-xs">BPJS Kesehatan</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="telkom.png" alt="" />
+                        <p className="text-xs">Telkom & Indihome</p>
+                    </div>
+                    <div className="m-4 p-3">
+                        <img className="h-17 w-auto object-center" src="tv.png" alt="" />
+                        <p className="text-xs">TV Berlangganan</p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
