6a32eedda875436ea365b7af68d5cad1fd1e5053
Fathoni Jati <fathoni.jati@corp.bm.co.id>
2022-12-23 13:43:35 +0700

    layout carousel and menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,22 @@
 import React from "react";
 import Layout from "./Layout";
 
+import Carousel from '../components/Carousel'
+import Menu from '../components/Menu'
+
 export default function Home(){
     return(
         <Layout >
             <div className="">
                 <div className="grid grid-cols-2 space-x-80">
                     <div className="ml-12 mt-10">
-                        <h1 className="text-4xl font-extrabold tracking-tight leading-none">Selamat Datang Di Sate Kuda</h1>
-                        <p className="text-lg font-normal">Nikmati sate daging kuda terlengkap dan termurah di Indonesia</p>
+
+                        {/* carousel fitur  */}
+                        <Carousel/>
+
+                        {/* menu fitur  */}
+
+                        <Menu />
                     </div>
                 </div>
             </div>
