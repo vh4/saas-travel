fc62cc3951256a9fbbaf44eb0717e913433190b9
Fathoni Jati <fathoni.jati@corp.bm.co.id>
2022-12-26 17:20:02 +0700

    hit-api transaksi lsit, profile, booking list, login, sigout

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const Routes = require("./routes/api")
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors({
+    origin:["http://localhost:3000"],
+    methods: ['GET', 'POST','DELETE', 'PUT'],
+    credentials:true
+})); 
+
+app.use(express.json());
+app.use(Routes);
+
+
+app.listen(port, ()=>{
+    console.log("server listening on port " + port);
+})
---
+++ routes/api.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const request = require('request');
+require('dotenv').config()
+
+const Router = express.Router();
+
+Router.post('/travel/app/sign_in', function (req, res) {
+
+    const {outletId, pin, key} = req.body;
+
+    request.post(
+        `${process.env.URL_HIT}/travel/app/sign_in`,
+        {
+          json: 
+          {
+            outletId: outletId,
+            pin: pin,
+            key: key
+          }
+        },
+        (error, response, body) => {
+          if (error) {
+            console.error(error)
+            return
+          }
+          return res.send(body)
+        }
+      )
+
+});
+
+Router.post('/travel/app/account', function (req, res) {
+
+    const {token} = req.body;
+
+    request.post(
+        `${process.env.URL_HIT}/travel/app/account`,
+        {
+          json: 
+          {
+            token: token,
+          }
+        },
+        (error, response, body) => {
+          if (error) {
+            console.error(error)
+            return
+          }
+          return res.send(body)
+        }
+      )
+
+});
+
+Router.post('/travel/app/sign_out', function (req, res) {
+
+    const {token} = req.body;
+
+    request.post(
+        `${process.env.URL_HIT}/travel/app/sign_out`,
+        {
+          json: 
+          {
+            token: token,
+          }
+        },
+        (error, response, body) => {
+          if (error) {
+            console.error(error)
+            return
+          }
+          return res.send(body)
+        }
+      )
+
+});
+
+Router.post('/travel/app/transaction_list', function (req, res) {
+
+    const {token, product} = req.body;
+
+    request.post(
+        `${process.env.URL_HIT}/travel/app/transaction_list`,
+        {
+          json: 
+          {
+            token: token,
+            product: product,
+          }
+        },
+        (error, response, body) => {
+          if (error) {
+            console.error(error)
+            return
+          }
+          return res.send(body)
+        }
+      )
+
+});
+
+Router.post('/travel/app/transaction_book_list', function (req, res) {
+
+    const {token, product} = req.body;
+
+    request.post(
+        `${process.env.URL_HIT}/travel/app/transaction_book_list`,
+        {
+          json: 
+          {
+            token: token,
+            product: product,
+          }
+        },
+        (error, response, body) => {
+          if (error) {
+            console.error(error)
+            return
+          }
+          return res.send(body)
+        }
+      )
+
+});
+
+Router.post('/travel/flight/search', function (req, res) {
+
+    const {token, airline, departure, arrival, departureDate, returnDate, 
+        isLowestPrice = true, adult, child, infant 
+    
+    } = req.body;
+
+    request.post(
+        `${process.env.URL_HIT}/travel/flight/search`,
+        {
+          json: 
+          {
+            token,
+            airline, departure, arrival, departureDate, returnDate, isLowestPrice,
+            adult, child, infant 
+          }
+        },
+        (error, response, body) => {
+          if (error) {
+            console.error(error)
+            return
+          }
+          return res.send(body)
+        }
+      )
+
+});
+
+module.exports = Router
