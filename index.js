40a0add0e9f4bfaebeeb72ad500485cdf8ba4053
toni <clear>
2023-09-15 16:55:48 +0700

    update env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const Routes = require("./routes/api")
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 9999;
 require('./utils/createFs');
 
 app.use(cors({
