3308791f90e05eaa6134ef990d4bbb3c600d638e
toni <clear>
2023-10-06 14:35:09 +0700

    update many requests

--- middleware/limit.js
+++ middleware/limit.js
@@ -1,26 +1,28 @@
-const { rateLimit } = require("express-rate-limit")
+const { rateLimit } = require("express-rate-limit");
 
 const apiLimiter = rateLimit({
-	windowMs: 1 * 60 * 1000, 
-	max: 5,
-	message: (req) => {
-		return { rc: '68', rd:'Terlalu banyak request.' };
-	  },	
-	standardHeaders: true,
-	legacyHeaders: false,
-})
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  handler: (req, res) => {
+    // Ubah response code menjadi 200
+    res.status(200).send({ rc: '68', rd: 'Terlalu banyak request.' });
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const apiLimiterKhususBooking = rateLimit({
-	windowMs: 1 * 60 * 1000, 
-	max: 6,
-	message: (req) => {
-		return { rc: '11', rd:'Terlalu banyak request booking kamu, mohon tunggu sekitar 2 menit.' };
-	  },	
-	standardHeaders: true,
-	legacyHeaders: false,
+  windowMs: 1 * 60 * 1000,
+  max: 6,
+  handler: (req, res) => {
+    // Ubah response code menjadi 200
+    res.status(200).send({ rc: '11', rd: 'Terlalu banyak request booking kamu, mohon tunggu sekitar 2 menit.' });
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 module.exports = {
-	apiLimiter,
-	apiLimiterKhususBooking
-}
+  apiLimiter,
+  apiLimiterKhususBooking,
+};
