b728e2c26c44a4ce017c4555de56408b579e4bd1
toni <clear>
2023-10-06 14:42:01 +0700

    update many requests pages

--- middleware/limit.js
+++ middleware/limit.js
@@ -12,8 +12,8 @@ const apiLimiter = rateLimit({
 });
 
 const apiLimiterKhususBooking = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 6,
+  windowMs: 2 * 60 * 1000,
+  max: 5,
   handler: (req, res) => {
     // Ubah response code menjadi 200
     res.status(200).send({ rc: '11', rd: 'Terlalu banyak request booking kamu, mohon tunggu sekitar 2 menit.' });
