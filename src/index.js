e2f1077a8d06d4802fc620068eadd2cfef4d1240
toni <clear>
2023-10-09 21:49:12 +0700

    docker nya ngk jalan

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-//docker error
+//docker error //
 axios.defaults.withCredentials = true
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
