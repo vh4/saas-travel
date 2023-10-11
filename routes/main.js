5dc14260f67998801bab421f36ddb7559da9e8de
toni <clear>
2023-10-11 10:56:03 +0700

    ganti captcha

--- routes/main.js
+++ routes/main.js
@@ -7,7 +7,7 @@ const Router = express.Router();
 
 Router.post('/travel/app/sign_in', async function (req, res) {
   const { username, password, token } = req.body;
-  const captcha_keys = '6LdGRpEoAAAAAAUGROG0BOUf1vl0uXUErtLl';
+  const captcha_keys = '6LdGRpEoAAAAAAUGROG0BOUf1vl0uXUErtLl-knf';
 
   logger.info(`Request /travel/app/sign_in: ${JSON.stringify(req.body)}`);
   logger.info(`Request HIT API RAJABILLER JSON: ${JSON.stringify({
