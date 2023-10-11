88122457fe9533b85ec32d9289e1beb058f45237
toni <clear>
2023-10-11 15:27:21 +0700

    update booking ke transaksi

--- src/components/components/pelniskeleton/bayar.jsx
+++ src/components/components/pelniskeleton/bayar.jsx
@@ -73,7 +73,8 @@ const BayarLoading = ({total}) => {
 					<div className="sidebar w-full xl:w-1/2">
 					<div className="mt-8 py-2 rounded-md border border-gray-200 shadow-sm">
 						<div className="px-4 py-2">
-						<div className="text-gray-500 text-xs">Status Booking</div>
+						{/* <div className="text-gray-500 text-xs">Status Booking</div> */}
+						<div className="text-gray-500 text-xs">Transaksi ID</div>
 						<div className="mt-1 font-bold text-blue-500 text-xs">
 							<Skeleton width={100} height={16} />
 						</div>
