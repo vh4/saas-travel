35a827862615f1569d43326548905bd577d9a5b1
toni <clear>
2023-12-07 09:11:42 +0700

    redirect login menu

--- routes/main.js
+++ routes/main.js
@@ -7,6 +7,82 @@ require('dotenv').config()
 const Router = express.Router();
 const country = getCountry();
 
+Router.post('/travel/app/redirect', async function (req, res) {
+  const {auth} = req.body;
+  try {
+
+    logger.info(`Request http://10.9.43.2:2023. data: ${auth}`); //
+
+    const dekript = await axios.post(`http://10.9.43.2:2023/index.php?dekrip=null`, {
+      dekrip:auth
+    });
+
+    logger.info(`Response http://10.9.43.2:2023. data: ${dekript.data}`);
+
+    if(dekript.data !== undefined && dekript.data !== null && dekript.data?.trim() !== '') {
+
+        const splitlogin = dekript.data.split('|');
+        logger.info(`Request /auth: username: ${splitlogin[0]} password: -------`); //
+
+        logger.info(`Request HIT RAJABILLER JSON: ${JSON.stringify({
+          username: splitlogin[0] || '',
+          method: "rajabiller.login_travel",
+          password: splitlogin[1] || ''
+        })}`); //
+
+        const response = await axios.post(process.env.HOST_AUTH, {
+          username: splitlogin[0] || '',
+          method: "rajabiller.login_travel",
+          password: splitlogin[1] || ''
+        });
+    
+
+        logger.info(`Response HIT RAJABILLER JSON: ${JSON.stringify(response?.data)}`);
+    
+        if (response.data.rc !== '00') {
+          return res.send({
+            rc: response.data.rc,
+            rd: response.data.rd
+          });
+        }
+    
+        const data = response.data;
+    
+        const expired = new Date(new Date().getTime() + 60 * 60 * 1000);
+        data.expired_date = expired;
+        data.username = splitlogin[0];
+
+        if (data.rc == '00') {
+          req.session['v_session'] = data.token;
+          req.session['expired_session'] = expired;
+    
+          logger.info(`INSERTING TOKEN TO SESSION: ${JSON.stringify(data.token)}`);
+    
+        }        
+    
+        logger.info(`Response /travel/app/sign_in: ${JSON.stringify(data)}`);
+        return res.send(data);
+
+    }else{
+
+      return res.send({
+        rc: '03',
+        rd: 'Login failed.'
+      });
+
+    }
+
+  } catch (error) {
+    logger.error(`Error /travel/app/sign_in: ${error.message}`);
+
+    return res.send({
+      rc: '03',
+      rd: error.message
+    });
+
+  }
+});
+
 Router.post('/travel/app/sign_in', async function (req, res) {
   const { username, password, token } = req.body;
   const captcha_keys = '6LdGRpEoAAAAAAUGROG0BOUf1vl0uXUErtLl-knf';
