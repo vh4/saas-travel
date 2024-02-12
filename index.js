fdcc78ee8760918af62348e53f3038770a3e1592
toni <clear>
2024-02-12 13:43:36 +0700

    fixing cors

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const url = process.env.FRONTEND_URL_OR_IP_ACCESS_CORS;
 logger.info(`.env production is alive. url hit frontend: ${url}`);
 
 app.use(cors({
-  origin:["*", "http://localhost:3000", "http://localhost:1111", url, "http://10.9.43.5:1111/","http://10.9.43.5:3000/"],
+  origin:["*", "http://localhost:3000", "http://localhost:1111", url, "http://10.9.43.5:1111","http://10.9.43.5:3000"],
   methods: ['GET', 'POST', 'DELETE', 'PUT'],
   credentials: true
 }));
